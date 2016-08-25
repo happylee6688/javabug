825713c3d7e8601d28efdef16d10ada7b2762408
lirongjiang <lirongjiang@qtone.cn>
2016-08-26 01:22:43 +0800

    添加了检测列表的xls表头

--- src/main/java/com/lrj/javabug/GdfdaProcess.java
+++ src/main/java/com/lrj/javabug/GdfdaProcess.java
@@ -30,9 +30,92 @@ public class GdfdaProcess {
 
         GdfdaService service = getService();
 
-        Call<ResponseBody> repo = service.getList();
+        JSONObject json = new JSONObject();
+        JSONObject params = new JSONObject();
+        params.put("javaClass", "ParameterSet");
+
+        JSONObject map = new JSONObject();
+        map.put("needTotal", true);
+        map.put("entId", "SYNCPKJMD00001026");
+        map.put("checkType", "2");
+        map.put("start  ", 0);
+        map.put("limit", 100);
+
+        params.put("map", map);
+        params.put("length", 8);
+
+        json.put("params", params);
+
+        JSONObject context = new JSONObject();
+        context.put("javaClass", "HashMap");
+        context.put("map", new JSONObject());
+        context.put("length", "HashMap");
+
+        json.put("context", context);
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), json.toString());
+        Call<ResponseBody> repo = service.getList(body);
+
+
 
         String list = getResultFromService(repo);
+
+        JSONObject resultJson = JSONObject.parseObject(list);
+        JSONArray resultArray = resultJson.getJSONArray("rows");
+
+        //处理excel结果
+        Workbook wb = new HSSFWorkbook();
+        Sheet sheet = wb.createSheet("快速检测单");
+        Row headerRow = sheet.createRow(0);
+        headerRow.setHeightInPoints(12.75f);
+        Cell cell_1 = headerRow.createCell(0);
+        cell_1.setCellValue("样品名称");
+        Cell cell_2 = headerRow.createCell(1);
+        cell_2.setCellValue("抽样日期");
+        Cell cell_3 = headerRow.createCell(2);
+        cell_3.setCellValue("抽样经手人");
+        Cell cell_4 = headerRow.createCell(2);
+        cell_4.setCellValue("被抽样经营者");
+        Cell cell_5 = headerRow.createCell(4);
+        cell_5.setCellValue("样标来源（省）");
+        Cell cell_6 = headerRow.createCell(5);
+        cell_6.setCellValue("样标来源（市）");
+        Cell cell_7 = headerRow.createCell(6);
+        cell_7.setCellValue("样标来源（县/区）");
+        Cell cell_8 = headerRow.createCell(7);
+        cell_8.setCellValue("样标来源（镇/街道）");
+        Cell cell_9 = headerRow.createCell(8);
+        cell_9.setCellValue("进货日期");
+        Cell cell_10 = headerRow.createCell(9);
+        cell_10.setCellValue("进货数量（kg）");
+        Cell cell_11 = headerRow.createCell(10);
+        cell_11.setCellValue("抽样数量（kg）");
+        Cell cell_12 = headerRow.createCell(11);
+        cell_12.setCellValue("供货者名称");
+        Cell cell_13 = headerRow.createCell(12);
+        cell_13.setCellValue("供货者地址");
+        Cell cell_14 = headerRow.createCell(13);
+        cell_14.setCellValue("生产者名称");
+        Cell cell_15 = headerRow.createCell(14);
+        cell_15.setCellValue("生产者地址");
+        Cell cell_16 = headerRow.createCell(15);
+        cell_16.setCellValue("检测总结论");
+        Cell cell_17 = headerRow.createCell(16);
+        cell_17.setCellValue("检测时间");
+        Cell cell_18 = headerRow.createCell(17);
+        cell_18.setCellValue("检测项目");
+        Cell cell_19 = headerRow.createCell(18);
+        cell_19.setCellValue("检测类型");
+        Cell cell_20 = headerRow.createCell(19);
+        cell_20.setCellValue("标准值");
+        Cell cell_21 = headerRow.createCell(20);
+        cell_21.setCellValue("计量单位");
+        Cell cell_22 = headerRow.createCell(21);
+        cell_22.setCellValue("检测值");
+        Cell cell_23 = headerRow.createCell(22);
+        cell_23.setCellValue("检测结论");
+        Cell cell_24 = headerRow.createCell(23);
+        cell_24.setCellValue("备注");
     }
 
     /**
